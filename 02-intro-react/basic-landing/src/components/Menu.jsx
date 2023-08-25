229ddbe437ff26ed2764d53e6d40bb0b775923e2
Juan Sebastian Zapata Ocampo <1144041080@u.icesi.edu.co>
2023-08-24 19:37:18 -0500

    useState in basic landing page

--- 02-intro-react/basic-landing/src/components/Menu.jsx
+++ 02-intro-react/basic-landing/src/components/Menu.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import "./Menu.css"
 export function Menu ({ menu, color }) {
   const menuStyles = {
     display: 'flex',
@@ -19,15 +21,51 @@ export function Menu ({ menu, color }) {
 }
 
 function Dropdown ({ item }) {
+
+  // ! useState devuelve un arreglo 
+  const [showList, setShowList] = useState(false)
   const { id, text, options } = item
+
+  // ! Estilos condicionales
+  const buttonStyle = {
+    backgroundColor: (showList ? "blue" : "white"),
+    color:  (showList ? "white" : "black")
+  }
+
+  // ! Ejemplo de setState recibiendo un valor
+  const showDropdownList = (e) => setShowList(true)
+
+  // ! Ejemplo de setState recibiendo un callback con el valor previo para manipularlo
+  const toggleState = () => setShowList((prevState) => !prevState)
+
+  // ! ClassName condicional para manejar estilos
+  const btnClassName = showList ? "isActive" : "" 
+
+  console.log("Me renderince Dropdown")
   return (
-    <li key={id}>
-      <button>{text}</button>
-      <DropdownList options={options} />
+    <li key={id} 
+        onMouseLeave={toggleState}>
+      <button type="button" 
+              onMouseEnter={showDropdownList} 
+              onClick={toggleState} 
+              className={btnClassName}
+              //style={buttonStyle}
+            >
+        {text}
+      </button>
+      { /* ! Renderizado condicional segun el valor del estado */ }
+      {
+        showList 
+          ? <DropdownList options={options} />
+          : null
+      }
     </li>
   )
 }
 function DropdownList ({ options }) {
+
+  console.log("Me renderince DropDownList")
+
   return (
     <ul>
       {
