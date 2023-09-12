8c56c8e67764bca411daad8dc20974731716fe94
Juan Sebastian Zapata Ocampo <1144041080@u.icesi.edu.co>
2023-09-11 20:02:38 -0500

    Implements custom hook in TodoList (0)

--- 03-react-hooks-events/todo-app/src/App.jsx
+++ 03-react-hooks-events/todo-app/src/App.jsx
@@ -1,80 +1,21 @@
-import { useState } from 'react'
 import { Form, List, Filters, Footer } from './components'
+import { filters } from './const/filters'
+import { useTodos } from './hooks/useTodos'
+const initTodosState = JSON.parse(window.localStorage.getItem('todos')) ?? []
 
 export default function App () {
-  const [todos, setTodos] = useState([])
-  const [filter, setFilter] = useState('all')
-
-  const filters = [
-    {
-      id: 1,
-      label: 'All',
-      value: 'all'
-    },
-    {
-      id: 2,
-      label: 'Completed',
-      value: 'completed'
-    },
-    {
-      id: 3,
-      label: 'Pending',
-      value: 'pending'
-    }
-  ]
-
-  const createTodo = (title) => {
-    const newTodo = {
-      id: crypto.randomUUID(),
-      title,
-      completed: false
-    }
-    setTodos(prevState =>
-      [...prevState, newTodo])
-  }
-
-  const handleToggle = (todo) => {
-    const { id, completed } = todo
-    const newTodos = todos.map(todo => {
-      if (todo.id === id) {
-        const newTodo = {
-          ...todo,
-          completed
-        }
-        return newTodo
-      }
-      return todo
-    })
-    setTodos(newTodos)
-  }
-
-  const handleDelete = (id) => {
-    const newTodos = todos.filter((item) => item.id !== id)
-    setTodos(newTodos)
-  }
-
-  const handleFilterChange = (filterValue) => {
-    setFilter(filterValue)
-  }
-
-  const filteredTodos = todos.filter(todo => {
-    if (filter === 'completed') {
-      return todo.completed
-    }
-    if (filter === 'pending') {
-      return !todo.completed
-    }
-    return todo
-  })
-
-  const hasTodos = todos.length > 0
-
-  const completedTodosCount = todos.filter((item) => item.completed).length
-
-  const clearAllCompleteTask = () => {
-    const newTasks = todos.filter((item) => !item.completed)
-    setTodos(newTasks)
-  }
+  const {
+    filter,
+    todos,
+    createTodo,
+    handleDelete,
+    handleToggle,
+    handleFilterChange,
+    filteredTodos,
+    hasTodos,
+    completedTodosCount,
+    clearAllCompleteTask
+  } = useTodos(initTodosState)
 
   return (
     <>
