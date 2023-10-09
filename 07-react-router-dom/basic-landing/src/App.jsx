0a6882386217202a40fadb56a9010007b1383eef
Juan Sebastian Zapata Ocampo <sebasz1000@gmail.com>
2023-10-09 15:24:39 -0500

    small code refactor

--- 07-react-router-dom/basic-landing/src/App.jsx
+++ 07-react-router-dom/basic-landing/src/App.jsx
@@ -1,16 +1,14 @@
-import React from "react"
-import {Header, Body, Footer} from "./components"
-function App(){
-
-    const imgUrl = "https://www.icesi.edu.co/biblioteca/wp-content/uploads/2022/02/logo_icesi.png"
-    return(
-        <>
-          <Header img={imgUrl} />
-          <Body />
-          <Footer />
-        </>
-    )
+import React from 'react'
+import { Header, Body, Footer } from './components'
+function App () {
+  const imgUrl = 'https://www.icesi.edu.co/biblioteca/wp-content/uploads/2022/02/logo_icesi.png'
+  return (
+    <>
+      <Header img={imgUrl} />
+      <Body />
+      <Footer />
+    </>
+  )
 }
 
 export default App
-
