54c9e8783ec9e99ab1e5aa2accf29c923bbd607d
Juan Sebastian Zapata Ocampo <sebasz1000@gmail.com>
2023-11-08 20:53:38 -0500

    Implements .env file for firebase config variables

--- 08-privated-routes-and-auth/basic-landing-SPA-with-auth/src/auth/config/firebase.js
+++ 08-privated-routes-and-auth/basic-landing-SPA-with-auth/src/auth/config/firebase.js
@@ -1,14 +1,22 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
+const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY
+const FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN
+const FIREBASE_PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID
+const FIREBASE_STORANGE_BUCKET = import.meta.env.VITE_FIREBASE_STORANGE_BUCKET
+const FIREBASE_MSG_SENDER_ID = import.meta.env.VITE_FIREBASE_MSG_SENDER_ID
+const FIREBASE_APP_ID = import.meta.env.VITE_FIREBASE_APP_ID
+const FIREBASE_MEASUREMENT_ID = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyA3S4Db885b0a3CwFJZMy1-PCokwrz6-Rg',
-  authDomain: 'basic-landing-spa.firebaseapp.com',
-  projectId: 'basic-landing-spa',
-  storageBucket: 'basic-landing-spa.appspot.com',
-  messagingSenderId: '495702965009',
-  appId: '1:495702965009:web:24b7c5eaec6732bbe7eab0',
-  measurementId: 'G-S20Y342RK1'
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORANGE_BUCKET,
+  messagingSenderId: FIREBASE_MSG_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
